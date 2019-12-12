645184528151daf4e7d87d1359e3de915d92bd7f
Filip Hesse <Filip_Hesse@yahoo.de>
2019-12-12 15:08:55 +0100

    Make start version compilable

--- src/turtle_go_to_goal.cpp
+++ src/turtle_go_to_goal.cpp
@@ -11,9 +11,9 @@ float EuclidianDistance(float x_g, float y_g, float x_r, float y_r)
   return sqrt(pow((x_g - x_r),2) + pow((x_g - x_r),2));
 }
 
-% Manage here the callback
+// Manage here the callback
 
-%
+//
 
 
 int main(int argc, char **argv)
@@ -22,26 +22,26 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
   float x_goal = 0;
   float y_goal = 0;
-  
+
   cout << "Insert X and Y coordinates that the robot should reach" <<endl;
   cin >> x_goal >> y_goal;
-  
+
   ros::Rate loop_rate(30);
   int counter = 0;
-  
-  % Manage here the publisher and the subscriber
-  
-  %
-  
+
+  // Manage here the publisher and the subscriber
+
+  //
+
   while (ros::ok())
   {
-    % Send here the velocity command to the robot
-    
-    
-    %
-  
-    % Exit conditions
-    counter++
+    // Send here the velocity command to the robot
+
+
+    //
+
+    // Exit conditions
+    counter++;
     if(counter>=900)
     {
       break;
@@ -49,7 +49,7 @@ int main(int argc, char **argv)
     if(EuclidianDistance(x_goal,y_goal,x_real,y_real)<0.5)
     {
       break;
-    }    
+    }
   }
   cout << "The process has finished in " << counter << " iterations"<<endl;
   cout << "The position error is " << EuclidianDistance(x_goal,y_goal,x_real,y_real) <<endl;
